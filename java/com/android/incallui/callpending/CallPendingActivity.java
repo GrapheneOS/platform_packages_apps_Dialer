b4873621bfacad9619ec9aa17ab1cdea0076bad1
Dmitry Muhomor <muhomor.dmitry@gmail.com>
2023-04-03 15:39:28 +0300

    callrec: remove no-op implementations of interfaces of call recording UI

--- java/com/android/incallui/callpending/CallPendingActivity.java
+++ java/com/android/incallui/callpending/CallPendingActivity.java
@@ -285,9 +285,6 @@ public class CallPendingActivity extends FragmentActivity
           public void swapSimClicked() {}
 
           @Override
-          public void callRecordClicked(boolean checked) {}
-
-          @Override
           public Context getContext() {
             return CallPendingActivity.this;
           }
