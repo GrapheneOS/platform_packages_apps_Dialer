07b6dc1261ee84bc73a2aa4ed25e3e643b8724f7
Dmitry Muhomor <muhomor.dmitry@gmail.com>
2023-04-03 15:39:28 +0300

    callrec: remove no-op implementations of interfaces of call recording UI

--- java/com/android/incallui/incall/protocol/InCallButtonUiDelegate.java
+++ java/com/android/incallui/incall/protocol/InCallButtonUiDelegate.java
@@ -65,7 +65,7 @@ public interface InCallButtonUiDelegate {
 
   void swapSimClicked();
 
-  void callRecordClicked(boolean checked);
+  default void callRecordClicked(boolean checked) {}
 
   Context getContext();
 }
